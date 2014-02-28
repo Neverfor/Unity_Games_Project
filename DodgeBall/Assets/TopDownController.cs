eb1724826d8e98eeb8df93e75cfeb4e8eb1a5185
Zwemvest <rik.oostveen@gmail.com>
2014-02-28 14:00:01 +0100

    dodgeball concept

---
+++ DodgeBall/Assets/TopDownController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TopDownController : MonoBehaviour {
+	private const float speed = 10;
+	public string verticalInput = "Vertical";
+	public string horizontalInput = "Horizontal";
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.position += Vector3.right * Time.deltaTime * Input.GetAxisRaw(horizontalInput) * speed;
+		this.transform.position += Vector3.forward * Time.deltaTime * Input.GetAxisRaw(verticalInput) * speed;
+	}
+}
