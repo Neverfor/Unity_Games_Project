6a567c2309fda79a6cc8ff75f7dfc7a70dc9eb6f
Neverfor <icelance@hotmail.ru>
2014-04-05 01:42:55 +0200

    Menu update
    menu

--- DodgeBall/Assets/Highscores.cs
+++ DodgeBall/Assets/Highscores.cs
@@ -13,11 +13,17 @@ public class Highscores : MonoBehaviour {
 		if(!string.IsNullOrEmpty(GameManager.winner))
 			GUI.Button(new Rect( 200, 100, 200, 20), GameManager.winner + " won the game");
 
-		if (GUI.Button (new Rect (200, 130, 200, 20), "Restart the game")) 
+		if (GUI.Button (new Rect (200, 130, 200, 20), "Try Lost Island Map!")) 
 		{
 			Application.LoadLevel("Scene2");
 			ScoreManager.ResetScores();
 		}
+
+        if (GUI.Button(new Rect(200, 160, 200, 20), "Try Space Game!"))
+        {
+            Application.LoadLevel("Scene3");
+            ScoreManager.ResetScores();
+        }
 	}
 
 }
