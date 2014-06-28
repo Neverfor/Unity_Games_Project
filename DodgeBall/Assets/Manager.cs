c792c135b92ce7cd379b38dac05dd2255d35ed45
Neverfor <icelance@hotmail.ru>
2014-06-28 02:03:55 +0200

    uitcomment code in manager (arrows)
    arrows outcommented

--- DodgeBall/Assets/Manager.cs
+++ DodgeBall/Assets/Manager.cs
@@ -58,18 +58,25 @@ using Assets;
         {
             if (seeTheBallHolder)
             {
+                /*
                 var p1 = GameObject.FindGameObjectWithTag("Player1");
                 var p2 = GameObject.Find("Player2");
                 if ("p1 heeft de bal".Equals("player 1 has the ball"))
                 {
-                    GameObject.Find("ArrowP1").active = true;
-                    GameObject.Find("ArrowP2").active = false;
+                    GameObject.Find("ArrowP1").renderer.enabled = true;
+                    GameObject.Find("ArrowP2").renderer.enabled = false;
                 }
                 if ("p2 heeft de bal".Equals("player 2 has the ball"))
                 {
-                    GameObject.Find("ArrowP1").camera.enabled = false;
-                    GameObject.Find("ArrowP2").camera.enabled = true;
+                    GameObject.Find("ArrowP1").renderer.enabled = false;
+                    GameObject.Find("ArrowP2").renderer.enabled = true;
                 }
+                 * */
+            }
+            else
+            {
+                GameObject.Find("ArrowP1").renderer.enabled = false;
+                GameObject.Find("ArrowP2").renderer.enabled = false;
             }
           
         }
